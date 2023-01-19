de4617ad43b7c0592e33d6e71518b4a1527c0847
naga272 <114398392+naga272@users.noreply.github.com>
2023-01-19 19:23:39 +0100

    Third deploy

---
+++ terzo_deploy_bf/bf/package/main.py
@@ -0,0 +1,11 @@
+from function import *
+
+'''
+    programma python con interfaccia grafica per programmare in brainfuck language
+    Bastianello Federico
+    03 / 01 / 2023
+'''
+
+if __name__ == "__main__":
+    log()
+    main()
